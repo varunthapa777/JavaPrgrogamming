a3799b22a63ba20febae934ed3d2df9685af1623
VarunThapa <140646101+varunthapa777@users.noreply.github.com>
2023-10-18 18:18:34 +0530

    Create SyncDemo.java

---
+++ MultiThreading/SyncDemo.java
@@ -0,0 +1,50 @@
+package MultiThreading;
+
+class MyData{
+
+    synchronized public void display(String str){
+// sychronized(this){
+        for(int i=0; i< str.length(); i++){
+
+            System.out.println(str.charAt(i));
+        }
+        //}
+    }
+}
+
+class MyThread1 extends Thread{
+
+    MyData d;
+
+    public MyThread1(MyData dat){ d = dat;}
+
+    public void run(){
+
+        d.display("Hello World ");
+    }
+}
+class MyThread2 extends Thread{
+
+    MyData data;
+
+    public MyThread2(MyData dat){ data = dat;}
+
+    public void run(){
+
+        data.display("Welcome");
+    }
+}
+
+public class SyncDemo {
+    
+    public static void main(String[] args) {
+        
+        MyData d = new MyData();
+        MyThread1 th1 = new MyThread1(d);
+        MyThread2 th2 = new MyThread2(d);
+
+        th1.start();
+        th2.start();
+
+    }
+}
